28792617bb7bbf8c4e1470a15a57d5c587f566d8
zzk19981124 <39255963+zzk19981124@users.noreply.github.com>
2021-11-24 18:03:11 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/demobluetoothmcu/MainActivity.java
+++ app/src/main/java/com/example/demobluetoothmcu/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //使用 https://github.com/Jasonchenlijian/FastBle/wiki
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
